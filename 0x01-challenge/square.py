917cf9f3e75f473c97f1d6f708b31f9b86124117
achrafezzaam <achraf.ezzaam@gmail.com>
2023-10-16 23:11:09 +0100

    Adding module and class documentation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
+""" Square Module """
 class Square():
-
+    """ Defining the Sqaure object """
     width = 0
     height = 0
 
@@ -24,7 +24,7 @@ class Square():
 
 
 if __name__ == "__main__":
-
+    """ Create a Square instance """
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())

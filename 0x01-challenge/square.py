526ebf96cfe17b1a0e40242503e53df531ab1c82
achrafezzaam <achraf.ezzaam@gmail.com>
2023-10-16 23:07:43 +0100

    adding proper documentation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-class square():
+class Square():
 
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Instantiate the Square object"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -13,16 +14,18 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Object representation """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
